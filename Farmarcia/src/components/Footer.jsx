86248b1fbfd83e69a5f736718696bd84c45136a6
rinmarys <rinmarysmonagas@gmail.com>
2024-10-01 16:02:29 -0300

    Atualizações feitas na Home Page

--- Farmarcia/src/components/Footer.jsx
+++ Farmarcia/src/components/Footer.jsx
@@ -5,8 +5,7 @@ function Footer() {
     return (
         <div className='footer'>
             <div className='container-um'>
-
-                <img src="farmarcia-logo-pq.PNG (1).png" alt="" />
+                <img src="farmarcia-logo-pq.PNG (1).png" alt="logo" />
                 <h3>Contatos</h3>
                 <div className='line'></div>
 
@@ -37,11 +36,11 @@ function Footer() {
             </div>
 
             <div className='container-tres'>
-            <h3>Endereço</h3>
-            <div className='line'></div>
-            <p>Farmárcia <br /> Rua das Palmeiras, 789 <br /> Bairro Coqueiros <br /> Florianópolis, SC, 88060-123</p>
+                <h3>Endereço</h3>
+                <div className='line'></div>
+                <p>Farmárcia <br /> Rua das Palmeiras, 789 <br /> Bairro Coqueiros <br /> Florianópolis, SC, 88060-123</p>
 
-            <p>Aviso: As informações da farmacia Farmárcia são fictícias e para fins de exemplo.</p>
+                <p>Aviso: As informações da farmacia Farmárcia são fictícias e para fins de exemplo.</p>
             </div>
         </div>
     )
